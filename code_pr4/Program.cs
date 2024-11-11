9be2b036bdaf4cdd1c7f9d28f85b9c23f0ad3111
Pliginkonstantin <falsekiller3@gmail.com>
2024-11-11 20:57:05 +0200

    code

---
+++ code_pr4/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace code_pr4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //task1 , сложение 3х вещественных чисел:
+            //double number_a, number_b, number_c;
+            //Console.Write("A = ");
+            //number_a = double.Parse(Console.ReadLine());
+            //Console.Write("B = ");
+            //number_b = double.Parse(Console.ReadLine());
+            //Console.Write("C = ");
+            //number_c = double.Parse(Console.ReadLine());
+            //Console.Write($"a + b + c = {number_a + number_b + number_c :f1}");
+            //Console.Read();
+
+            //3х целых чисел
+
+            //int number_a, number_b, number_c;
+            //Console.Write("A = ");
+            //number_a = int.Parse(Console.ReadLine());
+            //Console.Write("B = ");
+            //number_b = int.Parse(Console.ReadLine());
+            //Console.Write("C = ");
+            //number_c = int.Parse(Console.ReadLine());
+            //Console.Write($"a + b + c = {number_a + number_b + number_c}");
+            //Console.Read();
+
+
+
+            //======================================================================================================================
+            //task2,площадь и периметр прямоугольника 
+            //целые :
+
+
+
+            //int side1, side2;
+            //Console.Write("сторона 1 : ");
+            //side1 = int.Parse(Console.ReadLine());
+            //Console.Write("сторона 2 : ");
+            //side2 = int.Parse(Console.ReadLine());
+            //Console.Write($"===============================\nпериметр  : {side1 * 2 + side2 *2}\nплощадь   : {side1 * side2}");
+            //Console.Read();
+
+            //вещественные :
+
+            //double side1, side2;
+            //Console.Write("сторона 1 : ");
+            //side1 = double.Parse(Console.ReadLine());
+            //Console.Write("сторона 2 : ");
+            //side2 = double.Parse(Console.ReadLine());
+            //Console.Write($"===============================\nпериметр  : {side1 * 2 + side2 * 2}\nплощадь   : {side1 * side2}");
+            //Console.Read();
+
+
+
+            //======================================================================================================================
+            //task 3 , Даны катеты прямоугольного треугольника. Найти его гипотенузу.
+
+
+            //double cath1, cath2;
+            //Console.Write("Катет 1 :");
+            //cath1 = double.Parse(Console.ReadLine());
+            //Console.Write("Катет 2 :");
+            //cath2 = double.Parse(Console.ReadLine());
+            //Console.Write($"гипотенуза(в квадрате) : {cath1 * cath1 + cath2 * cath2}");
+            //Console.Read();
+
+
+            //======================================================================================================================
+
+
+            //task4
+            //целые:
+
+            //int side1;
+            //Console.Write("сторона   : ");
+            //side1 = int.Parse(Console.ReadLine());
+            //Console.Write($"===============================\nпериметр  : {side1 * 4}\nплощадь   : {side1 * side1}");
+            //Console.Read();
+
+            //вещественные :
+
+            //double side1;
+            //Console.Write("сторона   : ");
+            //side1 = double.Parse(Console.ReadLine());
+            //Console.Write($"===============================\nпериметр  : {side1 * 4:f1}\nплощадь   : {side1 * side1:f1}");
+            //Console.Read();
+
+
+
+
+        }
+    }
+}
